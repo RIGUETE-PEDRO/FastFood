8eb7151134d253ea6e4d2541b93a5c5d3c80185b
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <154231408+RIGUETE-PEDRO@users.noreply.github.com>
2026-02-18 14:39:51 -0300

    COMENTANDO E ARRUMANDO ROTAS

--- routes/web.php
+++ routes/web.php
@@ -15,43 +15,65 @@ use App\Http\Controllers\CarrinhoController;
 use App\Http\Controllers\PedidoController;
 use App\Http\Controllers\PedidosFeitosController;
 
-//rota da página inicial
-Route::get('/', [IndexController::class, 'index'])->name('home');
 
-//login
-Route::get('/login', function () {
-    return view('Login');
-})->name('login.form');
+///////////////////////////////////////////////////////
+/* GERENCIAMENTO DE PERFIL */
+///////////////////////////////////////////////////////
 
-//cadastro
-Route::get('/registro', function () {
-    return view('Cadastrar');
-})->name('registro.form');
+    Route::get('/', [IndexController::class, 'index'])->name('home');
 
-Route::get('/Lista_Produtos', [GerenciamentoProdutoController::class, 'gerenciamentoProduto'])->name('ListaProdutos');
-//cadastro de funcionário
-Route::post('/CadastrarFuncionario', [RegisterController::class, 'registerFuncionario'])
-    ->name('CadastrarFuncionario')
-    ->middleware(['auth', 'admin.access']);
+    //perfil
+    Route::post('/perfil', [AdminController::class, 'InfoPerfil'])->name('usuario')->middleware('auth');
 
-//esqueci minha senha
-Route::get('/esqueci-senha', function () {
-    return view('Esqueci-senha');
-})->name('senha.form');
+    //login
+    Route::get('/login', function () {
+        return view('Login');
+    })->name('login.form');
 
-//redifinir senha
-Route::get('/redefinir-senha', function () {
-    return view('Redefinir-senha');
-})->name('senha.redefinir.form');
+    //cadastro
+    Route::get('/registro', function () {
+        return view('Cadastrar');
+    })->name('registro.form');
 
+    //lista de produtos
+    Route::get('/Lista_Produtos', [GerenciamentoProdutoController::class, 'gerenciamentoProduto'])->name('ListaProdutos');
+
+    //cadastro de funcionário
+    Route::post('/CadastrarFuncionario', [RegisterController::class, 'registerFuncionario'])
+        ->name('CadastrarFuncionario')
+        ->middleware(['auth', 'admin.access']);
+
+    //esqueci minha senha
+    Route::get('/esqueci-senha', function () {
+        return view('Esqueci-senha');
+    })->name('senha.form');
+
+    //redifinir senha
+    Route::get('/redefinir-senha', function () {
+        return view('Redefinir-senha');
+    })->name('senha.redefinir.form');
+
+
+    //logout
+    Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    //perfil
+    Route::get('/perfil', [AdminController::class, 'InfoPerfil'])->name('perfil')->middleware('auth');
+
+    //buscar funcionários
+    Route::get('/funcionarios/buscar', [GerenciamentoUsuarioController::class, 'buscarFuncionarios'])->name('funcionarios.buscar');
+/////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////
+/* PROCESSO DOS PEDIDOS */
+///////////////////////////////////////////////////////
 //administrativo
 Route::get('/Administrativo', [AdminController::class, 'nomeUsuario'])
     ->name('Administrativo')
     ->middleware(['auth', 'admin.access']);
 
-Route::get('/gerenciamento_Funcionario', [GerenciamentoUsuarioController::class, 'gerenciamentoFuncionario'])
-    ->name('gerenciamento_funcionarios')
-    ->middleware(['auth', 'admin.access']);
+
 
 Route::get('/Pedidos.Administrativo', [PedidosFeitosController::class, 'verPedidosAdmin'])
     ->name('Pedidos.Administrativo')
@@ -65,21 +87,34 @@ Route::post('/Pedidos.Administrativo/{pedido}/avancar', [PedidosFeitosController
     ->name('Pedidos.StatusAvancar')
     ->middleware(['auth', 'admin.access']);
 
+Route::get('/pedidos', [PedidoController::class, 'verPedido'])->name('pedidos')->middleware('auth');
 
-//gerenciamento de produtos
+
+///////////////////////////////////////////////////////
+/* GERENCIAMENTO DE PRODUTOS */
+///////////////////////////////////////////////////////
 Route::post('/gerenciamento_Produtos', [GerenciamentoProdutoController::class, 'gerenciamentoProduto'])
     ->name('gerenciamento_produtos')
     ->middleware(['auth', 'admin.access']);
 
-//logout
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::post('/Cadastrar_Produto', [GerenciamentoProdutoController::class, 'cadastrarProduto'])
+    ->name('Cadastrar_Produto')
+    ->middleware(['auth', 'admin.access']);
 
-//perfil
-Route::get('/perfil', [AdminController::class, 'InfoPerfil'])->name('perfil')->middleware('auth');
+ Route::get('/gerenciamento_Produtos', [GerenciamentoProdutoController::class, 'gerenciamentoProduto'])
+    ->name('gerenciamento_Produtos')
+    ->middleware(['auth', 'admin.access']);
 
-//buscar funcionários
-Route::get('/funcionarios/buscar', [GerenciamentoUsuarioController::class, 'buscarFuncionarios'])->name('funcionarios.buscar');
+Route::post('/gerenciamento_Produtos/{id}/deletar', [GerenciamentoProdutoController::class, 'deletarProduto'])
+    ->name('deletar_produto')
+    ->middleware(['auth', 'admin.access']);
 
+
+
+
+///////////////////////////////////////////////////////
+/* GERENCIAMENTO DO USUARIO */
+///////////////////////////////////////////////////////
 Route::get('/Lanches',[LanchesController::class, 'Lanches'])->name('Lanches');
 
 Route::get('/Pizza', [PizzaController::class, 'Pizza'])->name('Pizza');
@@ -88,7 +123,10 @@ Route::get('/Bebidas', [BebidasController::class, 'Bebidas'])->name('Bebidas');
 
 Route::get('/porcao', [PorcaoController::class, 'porcao'])->name('Porcao');
 
-////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////
+/* GERENCIAMENTO DE CARRINHO */
+///////////////////////////////////////////////////////
 Route::get('/carrinho', [CarrinhoController::class, 'verCarrinho'])->name('carrinho')->middleware('auth');
 
 Route::post('/carrinho/adicionar', [CarrinhoController::class, 'adicionarAoCarrinho'])->name('carrinho.adicionar')->middleware('auth');
@@ -111,19 +149,33 @@ Route::get('/carrinho/{id}/deletar', function ($id) {
 
 Route::match(['post', 'delete'], '/carrinho/{id}/deletar', [CarrinhoController::class, 'deletarEndereco'])->name('endereco.excluir')->middleware('auth');
 
-//////////////////////////////////////////
-Route::get('/pedidos', [PedidoController::class, 'verPedido'])->name('pedidos')->middleware('auth');
 
+
+
+
+
+
+///////////////////////////////////////////////////////
+/* ACESSO NEGADO */
+///////////////////////////////////////////////////////
 Route::get('/AcessoNegado', function () {
     return view('Admin.PermisaoNegada');
 })->name('AcessoNegado');
 
 
-/////////////////////////////
 
 
 
-//atualizar funcionário
+
+
+///////////////////////////////////////////////////////
+/* GERENCIAMENTO DE FUNCIONARIO */
+///////////////////////////////////////////////////////
+
+Route::get('/gerenciamento_Funcionario', [GerenciamentoUsuarioController::class, 'gerenciamentoFuncionario'])
+    ->name('gerenciamento_funcionarios')
+    ->middleware(['auth', 'admin.access']);
+
 Route::post('/funcionarios/{id}/atualizar', [GerenciamentoUsuarioController::class, 'atualizarFuncionario'])
     ->name('funcionarios.atualizar')
     ->middleware(['auth', 'admin.access']);
@@ -137,28 +189,21 @@ Route::post('/atualizar_produto/{id}/atualizar', [GerenciamentoProdutoController
     ->middleware(['auth', 'admin.access']);
 
 //pegar dados do usuário logado
-Route::post('/perfil', [AdminController::class, 'InfoPerfil'])->name('usuario')->middleware('auth');
 
 
-//rotas de autenticação
+
+
+///////////////////////////////////////////////////////
+/* GERENCIAMENTO DE AUTENTICAÇÃO */
+///////////////////////////////////////////////////////
 Route::post('/registro', [RegisterController::class, 'register'])->name('registro');
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/esqueci-senha', [LoginController::class, 'recuperarSenha'])->name('senha.recuperar');
 Route::post('/redefinir-senha', [LoginController::class, 'atualizarSenha'])->name('senha.atualizar');
 Route::post('/Alterar_Dados', [AdminController::class, 'AlterarDados'])->name('Alterar_Dados');
 
-//rota de cadastro de produto
-Route::post('/Cadastrar_Produto', [GerenciamentoProdutoController::class, 'cadastrarProduto'])
-    ->name('Cadastrar_Produto')
-    ->middleware(['auth', 'admin.access']);
 
 
-Route::get('/gerenciamento_Produtos', [GerenciamentoProdutoController::class, 'gerenciamentoProduto'])
-    ->name('gerenciamento_Produtos')
-    ->middleware(['auth', 'admin.access']);
 
-Route::post('/gerenciamento_Produtos/{id}/deletar', [GerenciamentoProdutoController::class, 'deletarProduto'])
-    ->name('deletar_produto')
-    ->middleware(['auth', 'admin.access']);
 
 Route::post('/', [IndexController::class, 'index'])->name('index');
